079dc7b0161374c3a93fa8866e9196c93d59c081
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-08-07 01:11:28 -0400

    product added and payment removed

--- core/settings.py
+++ core/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # installed app 
     'home',
-    'payments',
+    'products',
     # third party app 
 ]
 
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIR = [
     STATIC_DIR,
 ] # Search during collectstatic
